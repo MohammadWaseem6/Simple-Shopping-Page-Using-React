3d6d573e1a7b634d735475f9c6f52ea8221c70f4
MohammadWaseem6 <wa6865688@gmail.com>
2024-02-18 13:05:36 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import HeroSection from './Components/HeroSection/HeroSection'
 import NavBar from './Components/NavBar/NavBar'
 function App() {
   return (
-   
-   <div>
-   <NavBar/>
-   <HeroSection/>
-   </div>
-    
+    <div>
+      <NavBar />
+      <HeroSection />
+    </div>
+
   )
 }
 
